8c15fe640026669a6d011fb530d06ede24cc3730
Danylo Ternovoi <danyloternovoi@gmail.com>
2024-07-10 13:37:55 +0300

    Initial Commit

---
+++ RoverVision/RoverVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoverVisionApp.swift
+//  RoverVision
+//
+//  Created by Danylo Ternovoi on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct RoverVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
